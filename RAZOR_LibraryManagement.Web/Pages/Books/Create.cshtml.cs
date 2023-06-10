de45161b37013b4c290989ad821ecc3ab22c5422
datecasp <ciudadano.fake@gmail.com>
2023-06-11 01:38:24 +0200

    upgraded navbar with dropdown menus instead of strings

---
+++ RAZOR_LibraryManagement.Web/Pages/Books/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RAZOR_LibraryManagement.Web.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
